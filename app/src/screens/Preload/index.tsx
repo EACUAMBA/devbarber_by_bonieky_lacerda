3ffb1d77b9eb4677c8b32a28f1d0afd0f0ab375d
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-09-18 19:53:58 +0200

    Configuração da biblioteca svg-trnasform e criação das screens de preload, signin e signup

--- app/src/screens/Preload/index.tsx
+++ app/src/screens/Preload/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Container, LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
+import BarberLogo from './../../assets/svg/barber.svg';
 
 const Preload = () => {
+  const navigation = useNavigation();
+
+  React.useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+
+    checkToken();
+  });
+
   return (
-    <View>
-      <Text>Preload</Text>
-    </View>
+    <Container>
+      <BarberLogo width="100%" height={160} />
+      <LoadingIcon size={'large'} color={'#FFFFFF'} />
+    </Container>
   );
 };
 
