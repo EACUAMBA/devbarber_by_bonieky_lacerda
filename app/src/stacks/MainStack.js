0e8fe972f6d8d4fb1194b0b40aeb7ca80db78423
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-11 00:00:53 +0200

    Created MainStack to group our screens and configured StackNavigator.

---
+++ app/src/stacks/MainStack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Preload from '../screens/Preload';
+import SignIn from './../screens/SignIn';
+import SignUp from './../screens/SignUp';
+
+const stackNavigator = createStackNavigator();
+
+export default () => (
+    <stackNavigator.Navigator
+        initialRouteName={'Preload'}
+        screenOptions={{
+            headerShown: false
+        }}
+    >
+        <stackNavigator.Screen name={"Preload"} component={Preload}/>
+        <stackNavigator.Screen name={"SignIn"} component={SignIn}/>
+        <stackNavigator.Screen name={"SignUp"} component={SignUp}/>
+    </stackNavigator.Navigator>
+);
