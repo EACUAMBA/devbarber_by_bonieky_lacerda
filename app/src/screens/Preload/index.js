ac423223285a8ec4f0f76ae71b7e2a128d2c6022
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-11 00:19:05 +0200

    Used the AsyncStorage to get and item from the storage, this item is he token and i will use to validate if the user session is still valid.

--- app/src/screens/Preload/index.js
+++ app/src/screens/Preload/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import {Container, LoadingIcon} from './styles';
 import DevBarber from './../../assets/barber.svg';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default () => {
+
+    useEffect(() => {
+       const checkToken = async () =>{
+           const token = await AsyncStorage.getItem("token");
+
+           if(token != null){
+               //validar se o token ainda é valido;
+               console.log("tens token papa 😎! ", token)
+           }else{
+               //enviar o individuo para a página de login
+               console.log("Não tens token papa 😭!")
+           }
+       }
+
+       checkToken();
+    }, []);
+
     return (
         <Container>
             <DevBarber width={"100%"} height={"160"}/>
