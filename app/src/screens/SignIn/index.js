989d32e42232564510165e9c3c3754c513c1eddd
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-11 00:58:09 +0200

    Added barber icon in SignIn screen.

--- app/src/screens/SignIn/index.js
+++ app/src/screens/SignIn/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {Container} from './styles';
+import DevBarber from './../../assets/barber.svg';
 
 export default () => {
     return (
         <Container>
-            <Text>SIGN IN</Text>
+            <DevBarber width={"100%"} height={"160px"}/>
         </Container>
     );
 }
