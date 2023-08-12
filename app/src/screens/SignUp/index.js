ada3b9e1c5d3c76ab8ccf52b152ef5f2216fda88
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-13 00:23:26 +0200

    Created the SignUp screen, it's just a copy past of SignIn screens with one input extra and a change in MessageButtonTextBold.

--- app/src/screens/SignUp/index.js
+++ app/src/screens/SignUp/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignInMessageButton,
+    SignInMessageButtonText,
+    SignInMessageButtonTextBold
+} from './styles';
+import DevBarber from './../../assets/barber.svg';
+import SignInput from './../../components/SignInput';
+import PersonIcon from './../../assets/person.svg';
+import EmailIcon from './../../assets/email.svg';
+import LockIcon from './../../assets/lock.svg';
 
 export default () => {
+    const navigation = useNavigation();
+
+    const [nameField, setNameField] = useState('Edilson A. Cuamba');
+    const [emailField, setEmailField] = useState('edilson@gmail.com');
+    const [passwordField, setPasswordField] = useState('');
+
+    function handleSignInMessageButtonClick(){
+        navigation.reset({
+            routes: [{name: 'SignIn'}]
+        });
+    }
+
+    function handleSignClick(){
+        console.log("handleSignClick");
+    }
+
     return (
         <Container>
-            <Text>SIGN UP</Text>
+            <DevBarber width={"100%"} height={"160px"}/>
+
+            <InputArea>
+                <SignInput IconSvg={PersonIcon} placeholder={"Digite o teu nome."} value={nameField} onChangeText={(text) => setNameField(text)}/>
+                <SignInput IconSvg={EmailIcon} placeholder={"Digite o teu email."} value={emailField} onChangeText={(text) => setEmailField(text)}/>
+                <SignInput IconSvg={LockIcon} placeholder={"Digite a tua senha."} value={passwordField} onChangeText={(text) => setPasswordField(text)} password={true}/>
+
+                <CustomButton onPress={handleSignClick}>
+                    <CustomButtonText>Login</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignInMessageButton onPress={handleSignInMessageButtonClick}>
+                <SignInMessageButtonText>Ainda não possui uma conta?</SignInMessageButtonText>
+                <SignInMessageButtonTextBold>Autentique-se!</SignInMessageButtonTextBold>
+            </SignInMessageButton>
         </Container>
     );
 }
--- app/src/screens/SignUp/styles.js
+++ app/src/screens/SignUp/styles.js
@@ -1,4 +1,54 @@
 import React from 'react';
 import {styled} from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  background-color: #63C2D1;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+/*
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignInMessageButton,
+    SignInMessageButtonText,
+    SignInMessageButtonTextBold
+ */
+
+export const InputArea = styled.View`
+  padding: 40px;
+  width: 100%;
+`;
+
+export const CustomButton = styled.TouchableOpacity`
+  height: 60px;
+  background-color: #268596;
+  border-radius: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+export const CustomButtonText = styled.Text`
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #FFF
+`;
+
+export const SignInMessageButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 20px;
+`;
+export const SignInMessageButtonText = styled.Text`
+  font-size: 16px;
+  color: #268596;
+`;
+export const SignInMessageButtonTextBold = styled.Text`
+  margin-left: 5px;
+  font-weight: bold;
+  font-size: 16px;
+  color: #268596;
+`;
