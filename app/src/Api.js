c529e06461d5ac6350e791b05520e97e0098ceae
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-15 00:19:09 +0200

    Created Api service responsible to agregate request api logic, now we have chekc token requets, sign in and signup.

---
+++ app/src/Api.js
@@ -0,0 +1,46 @@
+const BASE_URL = 'https://api.b7web.com.br/devbarber/api';
+
+export default {
+    checkToken: async (token) => {
+        const request = await fetch(`${BASE_URL}/auth/refresh`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token})
+        });
+
+        const json = await request.json();
+        return json;
+    },
+
+    signIn: async (email, password) => {
+        const request = await fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        });
+
+        const json = await request.json();
+        return json;
+    },
+
+    signUp: async (name, email, password) => {
+        const request = await fetch(`${BASE_URL}/user`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name, email, password})
+        });
+
+        const json = await request.json();
+        return json;
+    }
+
+};
