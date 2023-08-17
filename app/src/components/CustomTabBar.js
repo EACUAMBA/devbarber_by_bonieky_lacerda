e77116da1f1c7eb7684101f337c726484083d0a1
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-17 21:55:14 +0200

    Created partitially custom tab bar and setted it.

---
+++ app/src/components/CustomTabBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+import {styled} from 'styled-components/native';
+
+const TabArea = styled.View`
+  height: 60px;
+  background-color: #4EADBE;
+  flex-direction: row;
+`;
+
+export default () => {
+    return (
+        <TabArea>
+
+        </TabArea>
+    );
+}
--- app/src/stacks/MainTab.js
+++ app/src/stacks/MainTab.js
@@ -5,11 +5,13 @@ import Search from './../screens/Search';
 import Appointments from './../screens/Appointments';
 import Favorites from './../screens/Favorites';
 import Profile from './../screens/Profile';
+import CustomTabBar from '../components/CustomTabBar';
 
 const bottomTabNavigator = createBottomTabNavigator();
 
 export default () => (
-    <bottomTabNavigator.Navigator>
+    <bottomTabNavigator.Navigator tabBar={props => <CustomTabBar {...props} />}
+    >
         <bottomTabNavigator.Screen name={"Home"} component={Home}/>
         <bottomTabNavigator.Screen name={"Search"} component={Search}/>
         <bottomTabNavigator.Screen name={"Appointments"} component={Appointments}/>
