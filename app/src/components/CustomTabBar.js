71535ae2466a080abe9f31690377ffaf5e166185
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-17 22:09:42 +0200

    Added TabItems and goTo function to change the current screen to the clicked.

--- app/src/components/CustomTabBar.js
+++ app/src/components/CustomTabBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
 import {styled} from 'styled-components/native';
+
+import HomeIcon from './../assets/home.svg';
+import SearchIcon from './../assets/search.svg';
+import TodayIcon from './../assets/today.svg';
+import FavoriteIcon from './../assets/favorite.svg';
+import PersonIcon from './../assets/person.svg';
 
 const TabArea = styled.View`
   height: 60px;
@@ -8,10 +13,35 @@ const TabArea = styled.View`
   flex-direction: row;
 `;
 
-export default () => {
+const TabItem = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export default ({state, navigation}) => {
+
+    function goTo(screenName){
+        navigation.navigate(screenName);
+    }
+
     return (
         <TabArea>
-
+            <TabItem onPress={() => goTo('Home')}>
+                <HomeIcon width={"24"} height={"24"} fill={"#FFFFFF"}/>
+            </TabItem>
+            <TabItem onPress={() => goTo('Search')}>
+                <SearchIcon width={"24"} height={"24"} fill={"#FFFFFF"}/>
+            </TabItem>
+            <TabItem onPress={() => goTo('Appointments')}>
+                <TodayIcon width={"24"} height={"24"} fill={"#FFFFFF"}/>
+            </TabItem>
+            <TabItem onPress={() => goTo('Favorites')}>
+                <FavoriteIcon width={"24"} height={"24"} fill={"#FFFFFF"}/>
+            </TabItem>
+            <TabItem onPress={() => goTo('Profile')}>
+                <PersonIcon width={"24"} height={"24"} fill={"#FFFFFF"}/>
+            </TabItem>
         </TabArea>
     );
 }
