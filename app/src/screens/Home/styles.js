cfa2849f78acc1400e99ad947d182b5c0a0912d6
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-22 23:04:49 +0200

    Created raw components for Home screen.

--- app/src/screens/Home/styles.js
+++ app/src/screens/Home/styles.js
@@ -3,3 +3,13 @@ import {styled} from 'styled-components/native';
 export const Container = styled.SafeAreaView`
 
 `;
+
+export const Scroller = styled.ScrollView``;
+
+export const HeaderArea = styled.View``;
+export const HeaderTitle = styled.Text``;
+export const SearchButton = styled.TouchableOpacity``;
+
+export const LocationArea = styled.View``;
+export const LocationInput = styled.TextInput``;
+export const LocationFinder = styled.TouchableOpacity``;
